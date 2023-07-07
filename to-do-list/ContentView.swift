992ee1d882febd1d918cbbc956b80e5dd0a810af
Magistra <magistraagis@gmail.com>
2023-07-07 21:09:25 +0800

    Initial Commit

---
+++ to-do-list/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  to-do-list
+//
+//  Created by magistra aptam on 07/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ to-do-list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to-do-list
+//
+//  Created by magistra aptam on 07/07/23.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
